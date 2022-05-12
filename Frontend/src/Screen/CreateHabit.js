4742b951fdec4408827540eddf3adab8d11412ec
muhammad342 <muhammadashraf342@yahoo.com>
2022-05-12 09:57:32 +0500

    create habit api integrate

--- Frontend/src/Screen/CreateHabit.js
+++ Frontend/src/Screen/CreateHabit.js
@@ -7,18 +7,26 @@ import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { Checkbox } from 'antd';
+import axios from 'axios'
 
-import {Form } from 'react-bootstrap'
 const CreateHabit = ({children}) => {
   
     const [name,setName]=useState('')
     const [text,setText]=useState('')
+    const[days,setDays]=useState([])
     const history = useNavigate();
 
     const back = '< Back '
     const [start, setStart] = React.useState();
     const [end, setEnd] = React.useState();
 
+       
+   function onChange(checkedValues) {
+    console.log('checked = ', checkedValues);
+    setDays(checkedValues)
+  
+  }
     const handleChange = (newValue) => {
       setStart(newValue);
     
@@ -27,6 +35,13 @@ const CreateHabit = ({children}) => {
     const handleDate = (newValue) => {
         setEnd(newValue);
       };
+      const submitHandler=async()=>{
+        console.log(name)
+        console.log(start)
+        console.log(end)
+       const {data} = await axios.post("/habit/create",{name,start,end,days,text})
+      
+      }
   return (
     <>
     <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -67,57 +82,36 @@ const CreateHabit = ({children}) => {
         <Row className='my-3 ' >
             <h5>Frequency:</h5>
 <Row>
-    <Form style={{height:'15vh'}}> <div key={'inline-checkbox'} className="mb-3">
-      <Form.Check
-        inline
-        label="M"
-        name="group1"
-        type='checkbox'
-        id={'inline-checkbox-1'}
-      />
-      <Form.Check
-        inline
-        label="T"
-        name="group1"
-        type='checkbox'
-        id={'inline-checkbox-2'}
-      />
-      <Form.Check
-        inline
-        
-        label="W"
-        type='checkbox'
-        id={'inline-checkbox-3'}
-      />
-       <Form.Check
-        inline
-        
-        label="Th"
-        type='checkbox'
-        id={'inline-checkbox-3'}
-      />
-       <Form.Check
-        inline
-        
-        label="F"
-        type='checkbox'
-        id={'inline-checkbox-3'}
-      />
-       <Form.Check
-        inline
-        
-        label="S"
-        type='checkbox'
-        id={'inline-checkbox-3'}
-      />
-       <Form.Check
-        inline
-        
-        label="S"
-        type='checkbox'
-        id={'inline-checkbox-3'}
-      />
-    </div></Form>
+<Checkbox.Group style={{ width: '100vw' }} onChange={onChange}>
+     <Row>
+       <Col xs={2}>
+         <Checkbox value="Monday">M</Checkbox>
+       </Col>
+       <Col xs={2} >
+         <Checkbox value="Tuesday">T</Checkbox>
+      </Col>
+       <Col xs={2}>
+         <Checkbox value="Wednesday">W</Checkbox>
+       </Col>
+       <Col xs={2}>
+         <Checkbox value="Thursday">Th</Checkbox>
+       </Col>
+      <Col xs={2}>
+       <Checkbox value="Friday">F</Checkbox>
+       
+     </Col>
+     <Col xs={2}>
+       <Checkbox value="Saturday">S</Checkbox>
+       
+     </Col>
+     <Col xs={2}>
+       <Checkbox value="Sunday">S</Checkbox>
+       
+     </Col>
+    
+  </Row>
+</Checkbox.Group>
+   
    </Row>
         </Row>
         <Row>
@@ -135,7 +129,7 @@ const CreateHabit = ({children}) => {
            </Row>
         
     </Container>
-   <Button  style={{width:'100vw' , backgroundColor:'#91B9C5B2',height:'13vh'}} type='submit'>DONE</Button>
+   <Button  style={{width:'100vw' , backgroundColor:'#91B9C5B2',height:'13vh'}} type='submit' onClick={submitHandler}>DONE</Button>
     </>
   )
 }
