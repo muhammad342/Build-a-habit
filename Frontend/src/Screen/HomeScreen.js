25274818f78dc884144154f48ee2440610aa4b9c
muhammad342 <muhammadashraf342@yahoo.com>
2022-05-14 23:05:48 +0500

    logo

--- Frontend/src/Screen/HomeScreen.js
+++ Frontend/src/Screen/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
-import { Row,Col, Container } from 'react-bootstrap'
+import { Row,Col, Image } from 'react-bootstrap'
 
 const HomeScreen = () => {
     const history = useNavigate();
@@ -8,8 +8,9 @@ const HomeScreen = () => {
   return (  
   <>
  
-  <Row className='d-flex align-items-center my-auto' style={{height:'100vh',backgroundColor:'#2C3A6B',overflowX:'hidden'} }  onClick={()=>history("/FirstPage")}>
-      <Col style={{color:'white'}}>  <p className='text-center '>Hye There!</p>
+  <Row className='d-flex  align-items-center my-auto' style={{height:'100vh',backgroundColor:'#2C3A6B',overflowX:'hidden'} }  onClick={()=>history("/FirstPage")}>
+      <Col style={{color:'white'}} className='d-flex justify-content-center  align-items-center flex-column' > 
+      <Image src='../Logo.jpg' className='text-center mb-2'/> <p className='text-center '>Hye There!</p>
       <p className='text-center' >Welcome To Build-A-Habit</p></Col>
   
      
